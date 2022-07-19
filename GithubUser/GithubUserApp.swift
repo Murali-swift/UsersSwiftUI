0a6d23724c297c58dc4904685653a556808d18b9
Murali <mnallusamy@opentext.com>
2022-07-19 18:43:58 +0530

    Initial Commit

---
+++ GithubUser/GithubUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserApp.swift
+//  GithubUser
+//
+//  Created by Murali Nallusamy on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
